8a7aa51a06edefe087dd690d88b9e9792d841f7b
Rick <heiwitty.16@gmail.com>
2023-10-22 21:52:12 -0300

    botoes, bg anime rank

---
+++ script.js
@@ -0,0 +1,9 @@
+function btnjujutsu() { //botao Home -> jujutsu rank
+    window.location.href = "pages/jujutsuRank.html";
+}
+function btndbz() {//botao Home -> dragon ball rank
+    window.location.href = "pages/dbzRank.html";
+}
+function home() {//botao rankAnime -> Home
+    window.location.href = "/index.html";
+}
--- script/jujutsuRank.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('btnMenu').addEventListener('click', function() {
-    window.location.href = "/index.html";
-});
